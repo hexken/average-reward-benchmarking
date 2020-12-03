211452736daaa9230cb7f77e5a0a5097d4759fe1
Ken Tjhia <KenTjhia@gmail.com>
2020-12-02 19:41:27 -0700

    remove random_generator

--- agents/er_buffer.py
+++ agents/er_buffer.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-import numpy as np
+from random import sample
 
 Experience = namedtuple('Experience', ['state', 'action', 'reward', 'next_state'])
 
@@ -9,7 +9,6 @@ class ERBuffer:
         self.capacity = capacity
         self.buffer = []
         self.next_pos = 0
-        self.rand_generator = rand_generator
 
     def add(self, *args):
         if len(self.buffer) < self.capacity:
@@ -18,7 +17,7 @@ class ERBuffer:
         self.next_pos = (self.next_pos + 1) % self.capacity
 
     def sample_batch(self, batch_size):
-        return self.rand_generator.sample(self.buffer, batch_size)
+        return sample(self.buffer, batch_size)
 
     def __len__(self):
         return len(self.buffer)

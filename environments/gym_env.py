4b0dc4e08f6568ccf39be6d53f4978b5bb161143
Ken Tjhia <KenTjhia@gmail.com>
2020-12-04 02:59:22 -0700

    using gym-games

---
+++ environments/gym_env.py
@@ -0,0 +1,30 @@
+import gym
+from abc import ABCMeta, abstractmethod
+from environments.base_environment import BaseEnvironment
+
+
+class GymEnvBase(BaseEnvironment):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        super().__init__()
+        self.env = None
+
+    def env_init(self, game, env_info):
+        self.env = gym.make(game)
+        self.env.seed(env_info['seed'])
+
+    def env_start(self):
+        return self.env.reset()
+
+    def env_step(self, action):
+        obs, reward, term = self.env.step(action)
+        self.reward_obs_term = reward, obs, term
+        self.env.render()
+        return self.reward_obs_term
+
+
+class Catcher:
+    # TODO normalize is on by default (arg to CatcherEnv())
+    def env_init(selfself, env_info):
+        super().__init__('Catcher-PLE-v0', env_info)
